74fcddd183a5229285f6a2d63433727a90bf1045
Benjamin West <mrgenixus@gmail.com>
2012-06-07 07:16:02 -0600

    changed some code layout

--- demo.js
+++ demo.js
@@ -1,28 +1,22 @@
-namespace('im.lyt.general.errors',function($){
-	var ns = this;
+namespace('com.github.mrgenixus.namespace-info.errors',function($){
+	
+	this.display = true;
 
-	this.display_errors = true;
-
-	var error_messages; 
-	var reset_error_messages = function (){
-		error_messages = [];
+	var error_messages = (this.reset = function (){
 		jQuery('.message').remove();
-	}
-	ns.reset_error_messages = reset_error_messages;
-	this.reset_error_messages();
-	var new_message = function(message,tag,type){
-		if (ns.display_errors && message && message != ''){
+		return error_messages = [];
+	})()
 
-			$dismiss = jQuery('<div class="dismiss">x</div>')
+	var new_message = function(message,type){
+		if (this.display && message && message != ''){
+
+			$dismiss = jQuery('<div class="dismiss">&times;</div>')
 			$message = jQuery('<p class="message">').html(message);
 			$error = jQuery('<div class="message">').append($dismiss).append($message)
 			if (type) $error.addClass(type);
 			$error.on('dismiss',(function($error){
 				return function(){
 					$error.remove();
-					if (typeof(tag) != 'undefined') {
-						delete error_messages[tag];
-					}
 				};
 			})($error))
 			$dismiss.click(
@@ -37,28 +31,25 @@ namespace('im.lyt.general.errors',function($){
 			return $error
 		}
 	}
-	var new_error = function(errorString,tag){
-
-		return new_message(errorString,tag,'error');
+	var new_error = function(errorString){
+		return new_message(errorString,'error');
 	}
-	var new_success = function(successString,tag){
-		return new_message(successString,tag,'success');
+	var new_success = function(successString){
+		return new_message(successString,'success');
 	}
 
-	forms = namespace('im.lyt.general.forms')
-
-	forms.new_error = new_error
-	forms.new_success = new_success
-	forms.new_message = new_message
-
+	this.success = new_success; this.error = new_error; this.new = new_message;
 
 })
 
 
-namespace('im.lyt.general.forms',function($){
-	var ns = this;
+namespace('com.github.mrgenixus.namespace-info.forms',function($){
+
+	var error = this.ns_parent.namespace('errors');
+
 	var submit_form = function(action,formId){
-		reset_error_messages();
+		error.reset();
+
 		var $form  = jQuery("#" + formId + ", form[name='" + formId + "']");
 		var myForm = $form[0];
 
@@ -74,20 +65,17 @@ namespace('im.lyt.general.forms',function($){
 				if(rdata['code'] != 200){
 
 					//if undefined, this displays nothing
-					$form.prepend(this.new_error($form.data('incomplete'),formId));
+					$form.prepend(error.error($form.data('incomplete'),formId));
 
 					var elementModel = ( rdata.model && '[' + rdata.model + ']' ) || '';
 
 					rdata.validation && $.each(
 						rdata.validation,
 						function(i,v){
-							$(myForm.elements['data' + elementModel + '[' + i + ']']).before(this.new_error(v,i));
+							$(myForm.elements['data' + elementModel + '[' + i + ']']).before(error.error(v));
 						}
 					);
-					 
 				} else {
-					
-					//console.log(rdata['status'])
 					var redirectUrl = "/";
 					if(typeof(rdata['redirect-to']) != 'undefined') redirectUrl = rdata['redirect-to'];
 					
@@ -95,46 +83,53 @@ namespace('im.lyt.general.forms',function($){
 						redirectUrl = (redirect_root || '.') + redirectUrl;
 						document.location.href = redirectUrl;
 					}
+
 					$form.trigger('callback')
 				}
 			}
 		});
 	}
 
-	var initialized_forms = [];
-	ns.initialized_forms = function(){
-		return new Array(initialized_forms);
+	/*private:*/ var initialized_forms = [];
+
+	/*public:*/ this.initialized_forms = function(){
+		return initialized_forms.slice();
 	}
 
 	var init_form = function(i,v){
-		ns.initialized_forms.push(v)
+
+		initialized_forms.push(v)
+
 		if (v.tagName != 'FORM') return;
 
 		var action = $(v).data('action') || v.action + '_' + {'get':'json','post':'rest'}[v.method.toLowerCase()];;
-		var name = v.name;
 
-		$(v).on('submit.submit_form',(function(action,name){
+		//wrapping up acion, name in closure
+		var submit = (function(action,name){
 			return function(){
 				submit_form(action,name);
 			}
-		})(action,name)).on('submit.safety',function(){ return false;});;
+		})(action,v.name);
+
+		$(v).off('submit.safety').off('submit.submit_form') //make sure that we don't stop this from firing if we already bind, or someting;
+			.on('submit.submit_form',submit)
+			.on('submit.safety',function(){ return false; });
 
 		v.action = ''
 		//console.log(i,v,action,name)
 	}
+
+	//jQuery initialization:
 	$.fn.init_form = function(callback){
 		return this.on('callback',callback).each(init_form)
-		//else return this.each(init_form);
 	}
-	ns.init_form = function(selector,callback){
+	$(document).on('ready','form[data-enablejson="true"]',init_form);
 
+	//namespace, jQuery dependent methods
+
+	this.init_form = function(selector,callback){
 		return $(selector).init_form(callback);
 	}
-	$(document).ready(function(){
-
-		$('form[data-enablejson="true"]').each(init_form)
-	})
-
 	this.submit_form = function(element){
 		element = element || this;	
 		return $(element).init_form().submit();
@@ -143,5 +138,6 @@ namespace('im.lyt.general.forms',function($){
 		element = element || this
 		return element;
 	}
+
 
 },jQuery);
